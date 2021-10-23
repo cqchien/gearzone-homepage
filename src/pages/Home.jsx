4a8091acefb62d23ef72126915d59378090bacf9
cqchien <caochientp1@gmail.com>
2021-10-23 23:21:58 +0700

    UI Announcement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar.jsx";
 
 const HomePage = () => {
   return (
     <div>
+      <Announcement />
       <Navbar />
     </div>
   );
