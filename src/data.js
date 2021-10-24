6314ec1245af78f9f8582f8f5c3c6829628edae2
cqchien <caochientp1@gmail.com>
2021-10-24 12:50:00 +0700

    create Slider and mock data for slide

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.imgur.com/mGwa6Qb.jpg",
+    title: "SUMMER SALE",
+    desc: "Bàn phím cơ AKKO 3098B Multi-modes World Tour Tokyo R2.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/gshuZki.jpg",
+    title: "AUTUMN BEST SELLER",
+    desc: "Bàn phím cơ AKKO 3098 DS Ocean Star (Akko sw v2)",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.imgur.com/7fKgJ7W.jpg",
+    title: "BEST POPULARITY ",
+    desc: "Bàn phím cơ AKKO 3098 ASA – Los Angeles",
+    bg: "fbf0f4",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar.jsx";
+import Slider from "../components/Slider";
 
 const HomePage = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
