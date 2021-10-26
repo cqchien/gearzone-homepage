e9cdbf73f26e217390cd19544119cc1dfea3c04e
cqchien <caochientp1@gmail.com>
2021-10-26 17:47:37 +0700

    update UI Slider

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -9,33 +9,24 @@ export default function Slider() {
       emulateTouch={true}
       showStatus={false}
       showThumbs={false}
+      showArrows={true}
       autoPlay={true}
       infiniteLoop={true}
     >
       <div className="carousel_item">
         <div className="container">
           <div className="row">
-            <div className="col-7 carousel_item-left">
-              <img src="/images/slider1.png" alt="" />
+            <div className="col-6 carousel_item-left">
+              <img src="/images/img-slide-1.png" alt="" />
             </div>
-            <div className="col-5 carousel_item-right">
-              <p className="carousel_item-title">Asus Zenbook</p>
-              <p className="carousel_item-content">Giảm ngay <span>1.000.000đ</span> (từ ngày 30/4 - 19/05/2021)</p>
-              <button className="my-btn">Đặt hàng ngay</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="carousel_item">
-        <div className="container">
-          <div className="row">
-            <div className="col-7 carousel_item-left">
-              <img src="/images/slider1.png" alt="" />
-            </div>
-            <div className="col-5 carousel_item-right">
-              <p className="carousel_item-title">Asus Zenbook</p>
-              <p className="carousel_item-content">Giảm ngay <span>1.000.000đ</span> (từ ngày 30/4 - 19/05/2021)</p>
-              <button className="my-btn">Đặt hàng ngay</button>
+            <div className="col-6 carousel_item-right">
+              <p className="carousel_item-title">
+                Mechanical Keyboard Leopold FC900R PD
+              </p>
+              <p className="carousel_item-content">
+                Discount up to <span>200.000đ</span>
+              </p>
+              <button className="my-btn">Offer Now!</button>
             </div>
           </div>
         </div>
@@ -43,13 +34,33 @@ export default function Slider() {
       <div className="carousel_item">
         <div className="container">
           <div className="row">
-            <div className="col-7 carousel_item-left">
-              <img src="/images/slider1.png" alt="" />
+            <div className="col-6 carousel_item-left">
+              <img src="/images/img-slide-2.png" alt="" />
             </div>
-            <div className="col-5 carousel_item-right">
-              <p className="carousel_item-title">Asus Zenbook</p>
-              <p className="carousel_item-content">Giảm ngay <span>1.000.000đ</span> (từ ngày 30/4 - 19/05/2021)</p>
-              <button className="my-btn">Đặt hàng ngay</button>
+            <div className="col-6 carousel_item-right">
+              <p className="carousel_item-title">
+                Mouse Logitech G102 LightSync White
+              </p>
+              <p className="carousel_item-content">
+                Special offer up to <span>56%</span> when buying with head phone
+              </p>
+              <button className="my-btn">Offer Now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="carousel_item">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 carousel_item-left">
+              <img src="/images/img-slide-3.png" alt="" />
+            </div>
+            <div className="col-6 carousel_item-right">
+              <p className="carousel_item-title">Head Phone Logitech G633S</p>
+              <p className="carousel_item-content">
+                Free delivery nationwide (30/4 - 19/05/2021).
+              </p>
+              <button className="my-btn">Offer Now!</button>
             </div>
           </div>
         </div>
