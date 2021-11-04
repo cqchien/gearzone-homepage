6c96acae81addb3631d2706e68dcaabf5460f612
cqchien <caochientp1@gmail.com>
2021-11-04 16:39:35 +0700

    set product page

--- src/configs/router.js
+++ src/configs/router.js
@@ -2,11 +2,17 @@ import React from "react";
 import { ROUTES } from "../constant/routePath";
 
 const HomePage = React.lazy(() => import("../pages/Home"));
+const ProductsPage = React.lazy(() => import("../pages/Products"));
 
 export const routes = [
   {
     path: ROUTES.HOME,
     exact: true,
     component: () => <HomePage />,
+  },
+  {
+    path: ROUTES.PRODUCTS,
+    exact: true,
+    component: () => <ProductsPage />,
   },
 ];
---
+++ src/pages/Products.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const ProductsPage = () => {
+  return (
+    <>
+      <Header isScroll={true} />
+      <Footer />
+    </>
+  );
+};
+
+export default ProductsPage;
