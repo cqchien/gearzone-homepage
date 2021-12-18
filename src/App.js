08bd178257164e9836325334afeca92e32ce5e27
cqchien <caochientp1@gmail.com>
2021-12-18 22:00:16 +0700

    Footer and fix some UI color

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'antd/dist/antd.css';
 import "./style/index.scss";
 import TopBar from "./components/TopBar/TopBar";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* Suspense lets your components “wait” for something before they can render. */}
       <Suspense fallback="loading">
         <div className="App" id="app">
-          <Header isScroll={true} />
+          <Header />
+          <Footer />
         </div>
       </Suspense>
     </BrowserRouter>
